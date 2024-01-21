c9cefbcbd5e814240f0e5102ba78d5636775cd90
krona12 <krona126139@gmail.com>
2024-01-21 16:17:25 +0800

    解决了snake的多节移动

--- rtl/video_display.v
+++ rtl/video_display.v
@@ -36,43 +36,56 @@ parameter  H_DISP  = 11'd1280;                  //分辨率--行
 parameter  V_DISP  = 11'd720;                   //分辨率--列
 
 localparam SIDE_W  = 11'd40;                    //屏幕边框宽度
-localparam BLOCK_W = 11'd40;                    //方块宽度
+localparam BLOCK_W = 11'd20;                    //方块宽度
 localparam BLUE    = 24'b00000000_00000000_11111111;    //屏幕边框颜色 蓝色
 localparam WHITE   = 24'b11111111_11111111_11111111;    //背景颜色 白色
 localparam BLACK   = 24'b00000000_00000000_00000000;    //方块颜色 黑色
 
+localparam INI_X =11'd640 ;//初始位置
+localparam INI_Y =11'd360 ;
+
 //reg define
-reg [10:0] block_x = SIDE_W ;                             //方块左上角横坐标
-reg [10:0] block_y = SIDE_W ;                             //方块左上角纵坐标
+reg [10:0] block_x = INI_X ;                             //方块左上角横坐标
+reg [10:0] block_y = INI_Y ;                             //方块左上角纵坐标
+reg [10:0] SnakeSize=3;//定义蛇长度
+reg [10:0] Snake_Array[24:0][1:0]; // 定义蛇的每节的坐标数组
+
+
 reg [21:0] div_cnt;                             //时钟分频计数器
 reg        h_direct;                            //方块水平移动方向，1：右移，0：左移
 reg        v_direct;                            //方块竖直移动方向，1：向下，0：
 reg [1:0]direction;
 //wire define   
 wire move_en;                                   //方块移动使能信号，频率为100hz
-assign move_en = (div_cnt == 22'd742500) ? 1'b1 : 1'b0;
+reg MOEN;                                       //按键移动是能
+assign move_en = (div_cnt == 25'd74250) ? 1'b1 : 1'b0;
 //*****************************************************
 //**                    main code
 //*****************************************************
 
 always @(posedge pixel_clk or negedge sys_rst_n) begin
     if (!sys_rst_n) begin
+        MOEN=0;
     end
     else if (key[0] == 0) begin  // 上
         if(!(direction==2'd1))
             direction=2'd0;
+            MOEN=1;
     end
     else if (key[1] == 0) begin  // 下
         if(!(direction==2'd0))
              direction=2'd1;
+            MOEN=1;            
     end
     else if (key[2] == 0) begin  // 左
         if(!(direction==2'd3))
             direction=2'd2;
+            MOEN=1;
     end
     else if (key[3] == 0) begin  // 右
         if(!(direction==2'd2))
             direction=2'd3;
+            MOEN=1;
     end
     // 省略空的else部分
 end
@@ -88,64 +101,85 @@ always @(posedge pixel_clk ) begin
             div_cnt <= 22'd0;                   //计数达10ms后清零
     end
 end
-
-//当方块移动到边界时，改变移动方向
-// always @(posedge pixel_clk ) begin         
-//     if (!sys_rst_n) begin
-//         h_direct <= 1'b1;                       //方块初始水平向右移动
-//         v_direct <= 1'b1;                       //方块初始竖直向下移动
-//     end
-//     else begin
-//         if(block_x == SIDE_W - 1'b1)            //到达左边界时，水平向右
-//             h_direct <= 1'b1;               
-//         else                                    //到达右边界时，水平向左
-//         if(block_x == H_DISP - SIDE_W - BLOCK_W)
-//             h_direct <= 1'b0;               
-//         else
-//             h_direct <= h_direct;
-            
-//         if(block_y == SIDE_W - 1'b1)            //到达上边界时，竖直向下
-//             v_direct <= 1'b1;                
-//         else                                    //到达下边界时，竖直向上
-//         if(block_y == V_DISP - SIDE_W - BLOCK_W)
-//             v_direct <= 1'b0;               
-//         else
-//             v_direct <= v_direct;
-//     end
-// end
-
+integer index0;
+integer index1;
+integer index2;
+integer index3;
+integer index4;
 //根据方块移动方向，改变其纵横坐标
 always @(posedge pixel_clk or negedge sys_rst_n) begin         
     if (!sys_rst_n) begin
-        block_x <= SIDE_W;                     // 方块初始位置横坐标
-        block_y <= SIDE_W;                     // 方块初始位置纵坐标
+        for(index0=0; index0<SnakeSize; index0=index0+1) begin
+            Snake_Array[index0][0] = INI_X - index0 * BLOCK_W;
+            Snake_Array[index0][1] = INI_Y;
+        end
     end
-    else if (move_en) begin
+    else if (move_en&&MOEN) begin
         case (direction)
-            2'd0: block_y <= block_y - 1'b1;   // 方块向上移动
-            2'd1: block_y <= block_y + 1'b1;   // 方块向下移动
-            2'd2: block_x <= block_x - 1'b1;   // 方块向左移动
-            2'd3: block_x <= block_x + 1'b1;   // 方块向右移动
+            2'd0:
+            begin
+                for(index1=SnakeSize-1; index1>0; index1=index1-1) begin
+                    Snake_Array[index1][0] = Snake_Array[index1-1][0];
+                    Snake_Array[index1][1] = Snake_Array[index1-1][1];
+                end
+                Snake_Array[0][1] = Snake_Array[0][1] -BLOCK_W;//这里要注意是加BLOCK_W,之前设为1，导致蛇重合成一个方块了。还以为是蛇移动和draw频率的问题
+            end
+            2'b1:
+            begin
+                for(index2=SnakeSize-1; index2>0; index2=index2-1) begin
+                    Snake_Array[index2][0] = Snake_Array[index2-1][0];
+                    Snake_Array[index2][1] = Snake_Array[index2-1][1];
+                end
+                Snake_Array[0][1] = Snake_Array[0][1] + BLOCK_W;
+            end
+            2'd2:
+            begin
+                for(index3=SnakeSize-1; index3>0; index3=index3-1) begin
+                    Snake_Array[index3][0] = Snake_Array[index3-1][0];
+                    Snake_Array[index3][1] = Snake_Array[index3-1][1];
+                end
+                Snake_Array[0][0] = Snake_Array[0][0] -BLOCK_W; // 修正为x坐标
+            end
+            2'd3:
+            begin
+                for(index4=SnakeSize-1; index4>0; index4=index4-1) begin
+                    Snake_Array[index4][0] = Snake_Array[index4-1][0]; // 修正变量名称
+                    Snake_Array[index4][1] = Snake_Array[index4-1][1];
+                end
+                Snake_Array[0][0] = Snake_Array[0][0] + BLOCK_W; // 修正为x坐标
+            end
         endcase
     end
 end
 
 
-//给不同的区域绘制不同的颜色
-always @(posedge pixel_clk ) begin         
-    if (!sys_rst_n) 
+integer index_draw;
+reg found_match = 0; // 添加一个标志来指示是否找到匹配
+
+// 给不同的区域绘制不同的颜色
+always @(posedge pixel_clk) begin
+    if (!sys_rst_n) begin
         pixel_data <= BLACK;
-    else begin
-        if(  (pixel_xpos < SIDE_W) || (pixel_xpos >= H_DISP - SIDE_W)
-          || (pixel_ypos < SIDE_W) || (pixel_ypos >= V_DISP - SIDE_W))
-            pixel_data <= BLUE;                 //绘制屏幕边框为蓝色
-        else
-        if(  (pixel_xpos >= block_x) && (pixel_xpos < block_x + BLOCK_W)
-          && (pixel_ypos >= block_y) && (pixel_ypos < block_y + BLOCK_W))
-            pixel_data <= BLACK;                //绘制方块为黑色
-        else
-            pixel_data <= WHITE;                //绘制背景为白色
+    end else begin
+        if ((pixel_xpos < SIDE_W) || (pixel_xpos >= H_DISP - SIDE_W)
+            || (pixel_ypos < SIDE_W) || (pixel_ypos >= V_DISP - SIDE_W)) begin
+            pixel_data <= BLUE; // 绘制屏幕边框为蓝色
+        end else begin
+            found_match = 0; // 在每次像素时钟的边缘重置标志
+            for (index_draw = 0; index_draw < SnakeSize && !found_match; index_draw = index_draw + 1) begin
+                if (((pixel_xpos >= Snake_Array[index_draw][0]) && (pixel_xpos < Snake_Array[index_draw][0] + BLOCK_W))
+                    && ((pixel_ypos >= Snake_Array[index_draw][1]) && (pixel_ypos < Snake_Array[index_draw][1] + BLOCK_W))) begin
+                    pixel_data <= BLACK; // 绘制方块为黑色
+                    found_match = 1; // 标记找到匹配，防止将pixel_data设置为WHITE
+                end
+            end
+            if (!found_match) begin
+                pixel_data <= WHITE; // 如果没有找到匹配，则绘制背景为白色
+            end
+        end
     end
 end
+
+
 
 endmodule 
