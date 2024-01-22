387052ba435248cea5d869f3f4fcda3c2b4d90d3
krona12 <krona126139@gmail.com>
2024-01-22 21:40:45 +0800

    添加食物以及蛇增长逻辑。Verilog for循环变量不可综合，笑了

--- rtl/video_display.v
+++ rtl/video_display.v
@@ -40,25 +40,36 @@ localparam BLOCK_W = 11'd20;                    //方块宽度
 localparam BLUE    = 24'b00000000_00000000_11111111;    //屏幕边框颜色 蓝色
 localparam WHITE   = 24'b11111111_11111111_11111111;    //背景颜色 白色
 localparam BLACK   = 24'b00000000_00000000_00000000;    //方块颜色 黑色
-
+localparam GREEN =24'h006400 ;
+localparam Crimson = 24'hDC143C;
+localparam FOOD_COLOR =Crimson ;
 localparam INI_X =11'd640 ;//初始位置
 localparam INI_Y =11'd360 ;
+localparam StandardF =30'd742500;
+localparam speed =1 ;
+
+localparam FOOD_W =11'd20 ;//食物大小
+localparam FOOD_X=11'd400 ;
+localparam FOOD_Y=11'd400 ;
 
 //reg define
 reg [10:0] block_x = INI_X ;                             //方块左上角横坐标
 reg [10:0] block_y = INI_Y ;                             //方块左上角纵坐标
-reg [10:0] SnakeSize=3;//定义蛇长度
-reg [10:0] Snake_Array[24:0][1:0]; // 定义蛇的每节的坐标数组
+reg [3:0] SnakeSize=3;//定义蛇长度
+localparam MaxSize =16 ;
+reg [10:0] Snake_Array[MaxSize:0][1:0]; // 定义蛇的每节的坐标数组
+//food
+reg [10:0]Food_Array[1:0];
 
 
-reg [21:0] div_cnt;                             //时钟分频计数器
+reg [25:0] div_cnt;                             //时钟分频计数器
 reg        h_direct;                            //方块水平移动方向，1：右移，0：左移
 reg        v_direct;                            //方块竖直移动方向，1：向下，0：
 reg [1:0]direction;
 //wire define   
 wire move_en;                                   //方块移动使能信号，频率为100hz
 reg MOEN;                                       //按键移动是能
-assign move_en = (div_cnt == 25'd74250) ? 1'b1 : 1'b0;
+assign move_en = (div_cnt == StandardF*10/speed) ? 1'b1 : 1'b0;
 //*****************************************************
 //**                    main code
 //*****************************************************
@@ -95,7 +106,7 @@ always @(posedge pixel_clk ) begin
     if (!sys_rst_n)
         div_cnt <= 22'd0;
     else begin
-        if(div_cnt < 22'd742500) 
+        if(div_cnt < StandardF*10/speed) 
             div_cnt <= div_cnt + 1'b1;
         else
             div_cnt <= 22'd0;                   //计数达10ms后清零
@@ -109,46 +120,63 @@ integer index4;
 //根据方块移动方向，改变其纵横坐标
 always @(posedge pixel_clk or negedge sys_rst_n) begin         
     if (!sys_rst_n) begin
-        for(index0=0; index0<SnakeSize; index0=index0+1) begin
+        for(index0=0; index0<MaxSize; index0=index0+1) begin
+            if(index0<SnakeSize)begin
             Snake_Array[index0][0] = INI_X - index0 * BLOCK_W;
             Snake_Array[index0][1] = INI_Y;
+            end
         end
+        Food_Array[0]=FOOD_X;
+        Food_Array[1]=FOOD_Y;
     end
     else if (move_en&&MOEN) begin
         case (direction)
             2'd0:
             begin
-                for(index1=SnakeSize-1; index1>0; index1=index1-1) begin
+                for(index1=MaxSize-1; index1>0; index1=index1-1) begin
+                      if(index1<SnakeSize)begin
                     Snake_Array[index1][0] = Snake_Array[index1-1][0];
                     Snake_Array[index1][1] = Snake_Array[index1-1][1];
+                      end
                 end
                 Snake_Array[0][1] = Snake_Array[0][1] -BLOCK_W;//这里要注意是加BLOCK_W,之前设为1，导致蛇重合成一个方块了。还以为是蛇移动和draw频率的问题
             end
             2'b1:
             begin
-                for(index2=SnakeSize-1; index2>0; index2=index2-1) begin
+                for(index2=MaxSize-1; index2>0; index2=index2-1) begin
+                      if(index2<SnakeSize)begin
                     Snake_Array[index2][0] = Snake_Array[index2-1][0];
                     Snake_Array[index2][1] = Snake_Array[index2-1][1];
+                      end
                 end
                 Snake_Array[0][1] = Snake_Array[0][1] + BLOCK_W;
             end
             2'd2:
             begin
-                for(index3=SnakeSize-1; index3>0; index3=index3-1) begin
+                for(index3=MaxSize-1; index3>0; index3=index3-1) begin
+                      if(index3<SnakeSize)begin
                     Snake_Array[index3][0] = Snake_Array[index3-1][0];
                     Snake_Array[index3][1] = Snake_Array[index3-1][1];
+                      end
                 end
                 Snake_Array[0][0] = Snake_Array[0][0] -BLOCK_W; // 修正为x坐标
             end
             2'd3:
             begin
-                for(index4=SnakeSize-1; index4>0; index4=index4-1) begin
+                for(index4=MaxSize-1; index4>0; index4=index4-1) begin
+                      if(index4<SnakeSize)begin
                     Snake_Array[index4][0] = Snake_Array[index4-1][0]; // 修正变量名称
                     Snake_Array[index4][1] = Snake_Array[index4-1][1];
+                      end
                 end
                 Snake_Array[0][0] = Snake_Array[0][0] + BLOCK_W; // 修正为x坐标
             end
         endcase
+        //吃到食物
+        if(Snake_Array[0][0]==Food_Array[0]&&Snake_Array[0][1]==Food_Array[1])
+        begin
+            SnakeSize=SnakeSize+1;
+        end
     end
 end
 
@@ -165,16 +193,24 @@ always @(posedge pixel_clk) begin
             || (pixel_ypos < SIDE_W) || (pixel_ypos >= V_DISP - SIDE_W)) begin
             pixel_data <= BLUE; // 绘制屏幕边框为蓝色
         end else begin
+          //  
             found_match = 0; // 在每次像素时钟的边缘重置标志
-            for (index_draw = 0; index_draw < SnakeSize && !found_match; index_draw = index_draw + 1) begin
+            for (index_draw = 0; index_draw < MaxSize && !found_match; index_draw = index_draw + 1) begin
+                  if(index_draw<SnakeSize)begin
                 if (((pixel_xpos >= Snake_Array[index_draw][0]) && (pixel_xpos < Snake_Array[index_draw][0] + BLOCK_W))
                     && ((pixel_ypos >= Snake_Array[index_draw][1]) && (pixel_ypos < Snake_Array[index_draw][1] + BLOCK_W))) begin
                     pixel_data <= BLACK; // 绘制方块为黑色
                     found_match = 1; // 标记找到匹配，防止将pixel_data设置为WHITE
                 end
+                    end
             end
             if (!found_match) begin
-                pixel_data <= WHITE; // 如果没有找到匹配，则绘制背景为白色
+                 if((pixel_xpos>=Food_Array[0])&&(pixel_xpos<Food_Array[0]+FOOD_W)&&(pixel_ypos>=Food_Array[1])&&(pixel_ypos<Food_Array[1]+FOOD_W))
+                    begin
+                        pixel_data<=FOOD_COLOR;
+                    end
+                else
+                    pixel_data <= WHITE; // 如果没有找到匹配，则绘制背景为白色
             end
         end
     end
